ff3cf61cee877d21880b2a55ce58314b697027f7
Peter Caba <petercaba902@gmail.com>
2024-09-04 10:12:35 -0400

    Add files via upload

---
+++ 02_07_stripping_names.py
@@ -0,0 +1,23 @@
+# (tooManySpaces) = "        Python Power             "
+# print (tooMAnySpaces.strip()+ "|")
+
+# Apples_amount = 5
+
+# APPLES_AMOUNT = 10 
+
+# print (apples_Amount)
+
+# a = "sweet string"
+# number = 5
+# doudbleNumber = 5.0
+
+# a= (5,5,5)
+# b= (5,5,5)
+
+
+# # input, process, output
+
+# name = input("what is your name")
+# print(name)
+
+
