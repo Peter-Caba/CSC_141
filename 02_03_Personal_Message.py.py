b3ab948cca2f2f9dd3f17628ee9d868f111f9aec
Peter Caba <petercaba902@gmail.com>
2024-09-05 09:10:45 -0400

    Add files via upload

---
+++ 02_03_Personal_Message.py.py
@@ -0,0 +1,7 @@
+full_name = "Peter"
+last_name = "Caba"
+full_name = f"{full_name} {last_name}"
+print(full_name)
+
+message = f"Hello Humans Domain Expassion Shattered Universe, {full_name.title()}!"
+print(message)
