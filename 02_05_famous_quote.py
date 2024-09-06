9ef02f1795d6fa7dca2699e941142ad1fce42163
Peter Caba <petercaba902@gmail.com>
2024-09-06 11:30:14 -0400

    Add files via upload

---
+++ 02_05_famous_quote.py
@@ -0,0 +1,5 @@
+quote = "I’m the best ever. I’m the most brutal and vicious, and most ruthless champion there’s ever been. Everyone has a plan until they get punched in the mouth."
+Author = "Mike Tyson"
+
+print(f'"{quote}"')
+print(f'-{Author}')
