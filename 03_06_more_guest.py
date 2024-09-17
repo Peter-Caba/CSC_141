71261d6876fd506d014053354e26f621dface1e8
Peter Caba <petercaba902@gmail.com>
2024-09-17 08:19:11 -0400

    Add files via upload

---
+++ 03_06_more_guest.py
@@ -0,0 +1,24 @@
+
+names = ["peter", "gaby ","audri", "tyler", "deandre", "xavier"]
+names.remove('peter')
+
+print(names)
+names.append('tyler')
+
+
+names.remove('deandre')
+
+
+names.append('xavier')
+
+message = (f"hello tyler welcome to the dinner party.")
+
+print(message)
+
+message = (f"good bye audri see you next time.")
+
+print(message)
+
+message = (f"hello xavier and deandre you have been invited to the party")
+
+print(message)
---
+++ 03_11_intentional_error.py
@@ -0,0 +1,4 @@
+food = ['burger','pizza','peas','cheese']
+print(food[2])
+#Fix line [2]
+#Change [19] to [2]
