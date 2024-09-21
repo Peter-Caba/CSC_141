d59c92ab03be3caeb04f57c1dfca5cfb82305237
Peter Caba <petercaba902@gmail.com>
2024-09-20 22:16:08 -0400

    Add files via upload

---
+++ 04_02_animals.py
@@ -0,0 +1,3 @@
+animals = [ 'Bird', 'Raven', 'Eagle' ]
+print(f'A {animals} would make a great pet')
+print('All of the animals will make such great pets.')
---
+++ 04_13_buffet.py
@@ -0,0 +1,4 @@
+dimensions = ('Chicken',22,  'Rice', 'Beans', 'SandWich')
+print("Here is the list of items in the buffet:")
+for i in dimensions:
+    print("they have " + i)
