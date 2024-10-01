234bff57d1dea01df12a667d14ddf66261c0c5dc
Peter Caba <petercaba902@gmail.com>
2024-10-01 09:07:20 -0400

    Update 01_03_Infinite_skills.py

--- 01_03_Infinite_skills.py
+++ 01_03_Infinite_skills.py
@@ -4,5 +4,6 @@ reading, writing, chess, gaming, being a listener, meditation,
 checkers, bowling, care giver, being a nice person, supporting others
 goals, finding whats right or wrong, bettering a helpful person,
 helping out my friends and family, quick learner, finding peace, 
-always making new friends, learning new things everyday, but what 
-"""
+always making new friends, learning new things everyday, but the skills
+that I want to have to are having a better understanding with coding, 
+good time mangement, and staying positve """
