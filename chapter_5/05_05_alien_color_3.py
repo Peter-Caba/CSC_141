8e91241e263bc6fc8c721958634b217b529bd98a
Peter Caba <petercaba902@gmail.com>
2024-10-01 08:21:23 -0400

    Add files via upload

---
+++ chapter_5/05_05_alien_color_3.py
@@ -0,0 +1,41 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points for shooting the alien!")
+
+elif alien_color == 'yellow':
+    print("You just earned 10 ponits for shooting the alien!")
+
+elif alien_color == 'red':
+    print("You just earned 15 points for shooting the alien!")
+else:
+    print("Alien color not recongnized")
+ 
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("You just earned 5 points for shooting the green alien!")
+elif alien_color == 'yellow':
+
+    print("You just earned 10 points for shooting the yellow alien!")
+elif alien_color == 'red':
+
+    print("You just earned 15 points for shooting the red alien!")
+else:
+    print("Alien color not recognized.")
+
+
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("You just earned 5 points for shooting the green alien!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points for shooting the yellow alien!")
+elif alien_color == 'red':
+    print("You just earned 15 points for shooting the red alien!")
+else:
+    print("Alien color not recognized.")
+
+
+
+
